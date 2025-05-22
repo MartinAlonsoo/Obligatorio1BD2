d0120ba8ae6f23913912582efcec056ae552b9b0
MartinAlonso <MA291799@fi365.ort.edu.uy>
2025-05-22 16:03:19 -0300

    Parte 2 terminadaa

--- Parte2.2.sql
+++ Parte2.2.sql
@@ -9,7 +9,7 @@ IDENTIFIED BY dis1dis1
 DEFAULT TABLESPACE SYSTEM
 TEMPORARY TABLESPACE TEMP;
 
-CREATE USER jug1 
+CREATE USER JUG1 
 IDENTIFIED BY jug1jug1
 DEFAULT TABLESPACE SYSTEM
 TEMPORARY TABLESPACE TEMP;
@@ -18,8 +18,7 @@ TEMPORARY TABLESPACE TEMP;
 --DROP de usuarios
 DROP USER ADM1;
 DROP USER DIS1;
-DROP USER jug1;
-
+DROP USER JUG1;
 
 
 --Creamos los ROLES
@@ -37,5 +36,63 @@ DROP ROLE ADMINISTRADOR;
 DROP ROLE JUGADOR;
 DROP ROLE DISENIADOR;
 
+--Creacion de vistas
+
+CREATE OR REPLACE VIEW JugadorVistaSinContra AS
+SELECT nombrePais, email, nombre, 
+        fecha_registro, cantHoras, nombreRegion
+FROM Jugador;
+
+CREATE OR REPLACE VIEW InfoPropioJugador AS
+SELECT *
+FROM Jugador
+WHERE nombre = USER; -- no es PK pero es UNIQUE
+
+CREATE OR REPLACE VIEW PersonajeJugadorSinEspecie AS
+SELECT p.id, p.email_Jugador, p.fuerza, p.agilidad, 
+       p.intelifencia, p.vitalidad, p.resistencia, 
+       p.nivel, p.cantMonedas
+FROM Personaje p, Jugador j
+WHERE p.email_Jugador = j.email
+AND j.nombre = USER; -- no es PK pero es UNIQUE
+
+CREATE OR REPLACE VIEW PersonajeJugador AS
+SELECT p.id, p.email_Jugador, p.fuerza, p.agilidad, 
+       p.intelifencia, p.vitalidad, p.resistencia, 
+       p.nivel, p.cantMonedas, p.especie
+FROM Personaje p, Jugador j
+WHERE p.email_Jugador = j.email
+AND j.nombre = USER; -- no es PK pero es UNIQUE
+
 --Asignamos los privilegios a ROL ADMINISTRADOR
 GRANT CREATE SESSION TO ADMINISTRADOR;
+GRANT SELECT ON JugadorVistaSinContra TO ADMINISTRADOR;
+GRANT UPDATE ON JugadorVistaSinContra TO ADMINISTRADOR;
+
+--Asignamos los privilegios a ROL DISENIADOR
+GRANT CREATE SESSION TO DISENIADOR;
+GRANT SELECT ON Items TO DISENIADOR;
+GRANT UPDATE ON Items TO DISENIADOR;
+GRANT INSERT ON Items TO DISENIADOR;
+
+GRANT SELECT ON Zona TO DISENIADOR;
+GRANT UPDATE ON Zona TO DISENIADOR;
+GRANT INSERT ON Zona TO DISENIADOR;
+
+GRANT SELECT ON EnemigoJefe TO DISENIADOR;
+GRANT UPDATE ON EnemigoJefe TO DISENIADOR;
+GRANT INSERT ON EnemigoJefe TO DISENIADOR;
+
+GRANT SELECT ON Enemigo TO DISENIADOR;
+GRANT UPDATE ON Enemigo TO DISENIADOR;
+GRANT INSERT ON Enemigo TO DISENIADOR;
+
+--Asignamos los privilegios a ROL JUGADOR
+GRANT CREATE SESSION TO JUGADOR;
+GRANT SELECT ON InfoPropioJugador TO JUGADOR;
+GRANT UPDATE ON InfoPropioJugador TO JUGADOR;
+
+GRANT SELECT ON PersonajeJugadorSinEspecie TO JUGADOR;
+GRANT UPDATE ON PersonajeJugadorSinEspecie TO JUGADOR;
+
+GRANT SELECT ON PersonajeJugador TO JUGADOR;
