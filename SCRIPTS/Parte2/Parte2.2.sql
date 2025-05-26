dfd1c91bccedbab153bfa8722fd41450370a2b98
MartinAlonso <MA291799@fi365.ort.edu.uy>
2025-05-26 18:51:18 -0300

    Asignados los roles a los usuarios

--- SCRIPTS/Parte2/Parte2.2.sql
+++ SCRIPTS/Parte2/Parte2.2.sql
@@ -1,3 +1,9 @@
+--DROP de usuarios
+DROP USER ADM1;
+DROP USER DIS1;
+DROP USER JUG1;
+
+
 --Creamos usuarios para poder hacer pruebas
 CREATE USER ADM1 
 IDENTIFIED BY adm1adm1
@@ -15,10 +21,10 @@ DEFAULT TABLESPACE SYSTEM
 TEMPORARY TABLESPACE TEMP;
 
 
---DROP de usuarios
-DROP USER ADM1;
-DROP USER DIS1;
-DROP USER JUG1;
+-- DROP de ROLES
+DROP ROLE ADMINISTRADOR;
+DROP ROLE JUGADOR;
+DROP ROLE DISENIADOR;
 
 
 --Creamos los ROLES
@@ -31,11 +37,14 @@ NOT IDENTIFIED;
 CREATE ROLE DISENIADOR
 NOT IDENTIFIED;
 
--- DROP de ROLES
-DROP ROLE ADMINISTRADOR;
-DROP ROLE JUGADOR;
-DROP ROLE DISENIADOR;
 
+
+
+--Drop de vistas
+DROP VIEW JugadorVistaSinContra CASCADE CONSTRAINTS;
+DROP VIEW InfoPropioJugador CASCADE CONSTRAINTS;
+DROP VIEW PersonajeJugadorSinEspecie CASCADE CONSTRAINTS;
+DROP VIEW PersonajeJugador CASCADE CONSTRAINTS;
 --Creacion de vistas
 
 CREATE OR REPLACE VIEW JugadorVistaSinContra AS
@@ -50,7 +59,7 @@ WHERE nombre = USER; -- no es PK pero es UNIQUE
 
 CREATE OR REPLACE VIEW PersonajeJugadorSinEspecie AS
 SELECT p.id, p.email_Jugador, p.fuerza, p.agilidad, 
-       p.intelifencia, p.vitalidad, p.resistencia, 
+       p.inteligencia, p.vitalidad, p.resistencia, 
        p.nivel, p.cantMonedas
 FROM Personaje p, Jugador j
 WHERE p.email_Jugador = j.email
@@ -58,7 +67,7 @@ AND j.nombre = USER; -- no es PK pero es UNIQUE
 
 CREATE OR REPLACE VIEW PersonajeJugador AS
 SELECT p.id, p.email_Jugador, p.fuerza, p.agilidad, 
-       p.intelifencia, p.vitalidad, p.resistencia, 
+       p.inteligencia, p.vitalidad, p.resistencia, 
        p.nivel, p.cantMonedas, p.especie
 FROM Personaje p, Jugador j
 WHERE p.email_Jugador = j.email
@@ -96,3 +105,10 @@ GRANT SELECT ON PersonajeJugadorSinEspecie TO JUGADOR;
 GRANT UPDATE ON PersonajeJugadorSinEspecie TO JUGADOR;
 
 GRANT SELECT ON PersonajeJugador TO JUGADOR;
+
+
+
+-- Asignamos los roles a los usuarios
+GRANT DISENIADOR TO DIS1;
+GRANT JUGADOR TO JUG1;
+GRANT ADMINISTRADOR TO ADM1;
